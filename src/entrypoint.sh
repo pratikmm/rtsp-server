165ceab08d18a513d8e6cfc72dc7d9c745f7fa4f
pratikmm <pratikmm@hotmail.com>
2020-08-01 03:29:38 +0530

    removed py file execution, added comments

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,8 @@
+# compile code
 gcc -o stream  stream.c  `pkg-config --cflags --libs gstreamer-rtsp-server-1.0`
 
-python3 fetch_video.py
-
-
+#setting variable for Video location
 video_location="/src/earth.mp4"
+
+# execute code
 ./stream "( filesrc location=$video_location ! qtdemux ! h264parse ! rtph264pay name=pay0 pt=96 )"
